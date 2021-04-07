0bb0d99a2546352b0635a16ec4c9a6e8661f63d8
Zuldajri <w.matyas15@hotmail.fr>
2021-04-07 16:46:07 +0200

    Create download1.sh

---
+++ scripts/download1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo $(date) " - ############## Starting Script ####################"
+
+set -e
+
+export ADMIN_USER=$1
+
+python3 /var/lib/waagent/custom-script/download/0/sapansible/python/downloader/basket1.py --config /var/lib/waagent/custom-script/download/0/sapansible/ansible/output.json --dir /sapmnt1
